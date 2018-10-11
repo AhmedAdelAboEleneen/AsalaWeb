60fd26ba0f3c651a16a5d4277e0f75756874b87e
AhmedAdelAboEleneen <ahmedadela017@gmail.com>
2018-10-11 13:31:53 +0200

    add new category class

---
+++ src/main/java/asala/pages/AsalaAddNewCategoryPage.java
@@ -0,0 +1,12 @@
+package asala.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AsalaAddNewCategoryPage extends AsalaPageBase {
+
+	public AsalaAddNewCategoryPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
