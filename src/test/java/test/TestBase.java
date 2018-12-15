7c2028c621555ac43839b115afa6fa4f1aa44b19
AhmedAdelAboEleneen <ahmedadela017@gmail.com>
2018-12-16 01:06:51 +0200

    Add Headless browser

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -5,6 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -27,9 +30,19 @@ public class TestBase {
 		} else if (browserName.equalsIgnoreCase("chrome")) {
 
 			System.setProperty("webdriver.chrome.driver",
-					"/home/be-max/eclipse-workspace/asala_qc_dashboard/drivers/chromedriver");
+					System.getProperty("user.dir")+"/drivers/chromedriver");
 			driver = new ChromeDriver();
+
+		} else if (browserName.equalsIgnoreCase("headless")) {
+
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY ,System.getProperty("user.dir")+"/drivers/phantomjs");
+			String [] phantomJsArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS , phantomJsArgs);
+			driver = new PhantomJSDriver(caps);
 		}
+
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
