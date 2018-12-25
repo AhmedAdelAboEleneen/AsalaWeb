cac844d69cb710263025f521a4954f467270fc83
AhmedAdelAboEleneen <ahmedadela017@gmail.com>
2018-12-25 16:49:31 +0200

    Make Refactor

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
@@ -12,7 +11,6 @@ public class PageBase {
 	public WebDriver driver;
 	public JavascriptExecutor jse;
 	public Select select;
-	public Actions action;
 
 	// create constructor
 	public PageBase(WebDriver driver) {
