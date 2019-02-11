5b5ad296c8dbaae91eb7390ce748f10e19c1968f
ahmed adel <ahmed.adel@ibtikar.net.sa>
2019-02-11 13:52:37 +0200

    Change geckodriver

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -3,9 +3,6 @@ package test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import utilities.Helper;
@@ -23,7 +20,7 @@ public class TestBase {
         if (browserName.equalsIgnoreCase("firefox")) {
 
             System.setProperty("webdriver.gecko.driver",
-                    System.getProperty("user.dir") + "/drivers/geckodriver");
+                    System.getProperty("user.dir") + "/drivers/geckodriver.exe");
             driver = new FirefoxDriver();
 
         } else if (browserName.equalsIgnoreCase("chrome")) {
@@ -31,17 +28,7 @@ public class TestBase {
             System.setProperty("webdriver.chrome.driver",
                     System.getProperty("user.dir") + "/drivers/chromedriver");
             driver = new ChromeDriver();
-
-        } else if (browserName.equalsIgnoreCase("headless")) {
-
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setJavascriptEnabled(true);
-            caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, System.getProperty("user.dir") + "/drivers/phantomjs");
-            String[] phantomJsArgs = {"--web-security=no", "--ignore-ssl-errors=yes"};
-            caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
-            driver = new PhantomJSDriver(caps);
         }
-
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
